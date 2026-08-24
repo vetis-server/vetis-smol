17d02dd52c5d8c9d477e0e4721c3a8a33d3f8c99
Rogério Araújo <rogerio.araujo@gmail.com>
2026-08-24 15:26:08 -0300

    test: provide correct path for cert and private key

--- tests/vetis_macros/mod.rs
+++ tests/vetis_macros/mod.rs
@@ -50,7 +50,7 @@ async fn test_http_localhost() -> Result<(), Box<dyn std::error::Error>> {
 #[apply(test!)]
 async fn test_https() -> Result<(), Box<dyn std::error::Error>> {
     let handler = handler_fn(|_req| async move { Ok(Response::builder().text("Hello, World!")) });
-
+    let root= env!("CARGO_MANIFEST_DIR");    
     let mut server = http!(
         from_crate => vetis_smol,
         hostname => "localhost",
@@ -60,9 +60,9 @@ async fn test_https() -> Result<(), Box<dyn std::error::Error>> {
         interface => "0.0.0.0".parse().unwrap(),
         handler => handler,
         security_config => security! {
-            cert => "../certs/server.der",
-            key => "../certs/server.key.der",
-            ca_cert => "../certs/ca.der",
+            cert => &format!("{root}/certs/server.der"),
+            key => &format!("{root}/certs/server.key.der"),
+            ca_cert => &format!("{root}*/certs/ca.der"),
             client_auth => false
         }
     )
@@ -72,7 +72,7 @@ async fn test_https() -> Result<(), Box<dyn std::error::Error>> {
         .start()
         .await?;
 
-    let certificate = DeboaCertificate::from_file("../certs/ca.der", ContentEncoding::DER).await?;
+    let certificate = DeboaCertificate::from_file(&format!("{root}/certs/ca.der"), ContentEncoding::DER).await?;
 
     let client = Client::builder()
         .certificate(certificate)
